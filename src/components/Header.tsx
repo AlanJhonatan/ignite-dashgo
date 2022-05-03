f392e6af42071d9ef8aa91fe8072b69a35a96eb0
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-05-02 21:19:51 -0300

    chore: finished header with mocked data

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react";
-import { RiSearchLine } from 'react-icons/ri';
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header() {
   return (
@@ -45,6 +45,33 @@ export function Header() {
           _placeholder={{ color: 'gray.400' }}
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto" >
+        <HStack 
+          spacing="4"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine}></Icon>
+          <Icon as={RiUserAddLine}></Icon>
+        </HStack>
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Alan Jhonatan</Text>
+            <Text color="gray.300">alan.amorim.franca@gmail.com</Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Alan Jhonatan"
+            src="https://github.com/alanjhonatan.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   )
