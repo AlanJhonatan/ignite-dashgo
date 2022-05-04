8aa7f42f2a9c448a29602e05e0b791a9dc49ec2c
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-05-03 21:43:31 -0300

    feature: added breakpoints to mobile devices

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,33 +1,24 @@
-import { Avatar, Box, Flex, HStack, Icon, Text } from "@chakra-ui/react";
-import { RiNotificationLine, RiUserAddLine } from "react-icons/ri";
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
-    <Flex align="center" ml="auto" >
-        <HStack 
-          spacing="4"
-          mx="8"
-          pr="8"
-          py="1"
-          color="gray.300"
-          borderRightWidth={1}
-          borderColor="gray.700"
-        >
-          <Icon as={RiNotificationLine}></Icon>
-          <Icon as={RiUserAddLine}></Icon>
-        </HStack>
-        <Flex align="center">
-          <Box mr="4" textAlign="right">
-            <Text>Alan Jhonatan</Text>
-            <Text color="gray.300">alan.amorim.franca@gmail.com</Text>
-          </Box>
+    <Flex align="center">
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Alan Jhonatan</Text>
+          <Text color="gray.300">alan.amorim.franca@gmail.com</Text>
+        </Box>
+      )}
 
-          <Avatar
-            size="md"
-            name="Alan Jhonatan"
-            src="https://github.com/alanjhonatan.png"
-          />
-        </Flex>
-      </Flex>   
+      <Avatar
+        size="md"
+        name="Alan Jhonatan"
+        src="https://github.com/alanjhonatan.png"
+      />
+    </Flex>
   )
 }
