aef0ab3ef0df88851d96ac29bc3190b9c07a9b7e
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-05-11 09:07:38 -0300

    chore: using react-query to fetch data

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,21 +1,29 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
 import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from "react-query";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json();
+
+    return data;
+  });
+
+  // console.log(Query)
+  
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then((response) => response.json())
-      .then((data) => console.log(data))
+    
     console.log('calling')
   }, [])
   
@@ -42,100 +50,29 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha" >
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                { isWideVersion && <Th>Data de Cadastro</Th>}
-                { isWideVersion && <Th width={8}></Th>}
-              </Tr>
-            </Thead>
+          { isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao carrega dados do usuário.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha" >
+              <Thead>
+                <Tr>
+                  <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                    <Checkbox colorScheme="pink" />
+                  </Th>
+                  <Th>Usuário</Th>
+                  { isWideVersion && <Th>Data de Cadastro</Th>}
+                  { isWideVersion && <Th width={8}></Th>}
+                </Tr>
+              </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Alan Jhonatan</Text>
-                    <Text fontWeight="bold" fontSize="sm" color="gray.300">
-                      alan.amorim.franca@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>04 de Abril de 2022</Td>}
-                <Td textAlign="end">
-                  {isWideVersion && (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16"   />}
-                    >
-                      Editar
-                    </Button>
-                  )}
-                </Td>
-              </Tr>
-              <Tr>
-                <Td>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Alan Jhonatan</Text>
-                    <Text fontWeight="bold" fontSize="sm" color="gray.300">
-                      alan.amorim.franca@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>04 de Abril de 2022</Td>}
-                <Td textAlign="end">
-                  {isWideVersion && (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16"   />}
-                    >
-                      Editar
-                    </Button>
-                  )}
-                </Td>
-              </Tr>
-              <Tr>
-                <Td>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Alan Jhonatan</Text>
-                    <Text fontWeight="bold" fontSize="sm" color="gray.300">
-                      alan.amorim.franca@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>04 de Abril de 2022</Td>}
-                <Td textAlign="end">
-                  {isWideVersion && (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16"   />}
-                    >
-                      Editar
-                    </Button>
-                  )}
-                </Td>
-              </Tr>
+              <Tbody>
                 <Tr>
                   <Td>
                     <Checkbox colorScheme="pink" />
@@ -163,9 +100,92 @@ export default function UserList() {
                     )}
                   </Td>
                 </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+                <Tr>
+                  <Td>
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">Alan Jhonatan</Text>
+                      <Text fontWeight="bold" fontSize="sm" color="gray.300">
+                        alan.amorim.franca@gmail.com
+                      </Text>
+                    </Box>
+                  </Td>
+                  { isWideVersion && <Td>04 de Abril de 2022</Td>}
+                  <Td textAlign="end">
+                    {isWideVersion && (
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                        leftIcon={<Icon as={RiPencilLine} fontSize="16"   />}
+                      >
+                        Editar
+                      </Button>
+                    )}
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">Alan Jhonatan</Text>
+                      <Text fontWeight="bold" fontSize="sm" color="gray.300">
+                        alan.amorim.franca@gmail.com
+                      </Text>
+                    </Box>
+                  </Td>
+                  { isWideVersion && <Td>04 de Abril de 2022</Td>}
+                  <Td textAlign="end">
+                    {isWideVersion && (
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                        leftIcon={<Icon as={RiPencilLine} fontSize="16"   />}
+                      >
+                        Editar
+                      </Button>
+                    )}
+                  </Td>
+                </Tr>
+                  <Tr>
+                    <Td>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Alan Jhonatan</Text>
+                        <Text fontWeight="bold" fontSize="sm" color="gray.300">
+                          alan.amorim.franca@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    { isWideVersion && <Td>04 de Abril de 2022</Td>}
+                    <Td textAlign="end">
+                      {isWideVersion && (
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16"   />}
+                        >
+                          Editar
+                        </Button>
+                      )}
+                    </Td>
+                  </Tr>
+              </Tbody>
+            </Table>
+            <Pagination />
+          </>
+          )}
         </Box>
       </Flex>
     </Box>
