d3095c736ceec343e2748a645f67d6b154d017da
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-05-13 21:41:41 -0300

    chore: integrating react-query with server side rendering

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,7 +1,7 @@
-import { useQuery } from "react-query";
+import { useQuery, UseQueryOptions } from "react-query";
 import { api } from "../api";
 
-type User = {
+export type User = {
   id: string;
   name: string;
   email: string;
@@ -39,8 +39,9 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
   };
 }
 
-export function useUsers(page: number) {
+export function useUsers(page: number, options: UseQueryOptions) {
   return useQuery(['users', page], () => getUsers(page), {
+    ...options,
     staleTime: 1000 * 60 * 10, // 10 minutes
   });
 }
